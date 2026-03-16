5ce836227f6cb1ce62bc6a8b85cc846df4303ca2
Rubén <74114026+rubzip@users.noreply.github.com>
2026-03-16 12:21:45 +0100

    Create engine.py

---
+++ backend/app/engine.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from app.schemas import Query
+
+
+def get(df: pd.DataFrame, q: Query) -> pd.DataFrame:
+    out = df.copy()
+    
+    if q.where:
+        pass
+    if q.select:
+        pass
+    if q.dropna:
+        pass
+    if q.drop_duplicates:
+        pass
+    if q.limit:
+        pass
+    if q.offset:
+        pass
+    if q.select:
+        pass
+
+    return out
+
+def get_code(df: pd.DataFrame, q: Query) -> List[str]:
+    out = []
+    
+    if q.where:
+        pass
+    if q.select:
+        pass
+    if q.dropna:
+        pass
+    if q.drop_duplicates:
+        pass
+    if q.limit:
+        pass
+    if q.offset:
+        pass
+    if q.select:
+        pass
+
+    return out
