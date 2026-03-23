cfb9ae23a0a20a9a4775d3fa4030304066752f31
Rubén <74114026+rubzip@users.noreply.github.com>
2026-03-23 16:46:22 +0100

    Update base.py

--- backend/app/utils/base.py
+++ backend/app/utils/base.py
@@ -1,17 +1,12 @@
 from abc import ABC, abstractmethod
-from typing import Any, List, Set
+from typing import List, Set
 import pandas as pd
 
 
-class BaseAction(ABC):
+class BaseComponent(ABC):
     """
     Abstract class for DataFrame transformations. Contains logic and code transforamtion.
     """
-    
-    @abstractmethod
-    def apply(self, df: pd.DataFrame) -> pd.DataFrame:
-        """Applies logic to DataFrame."""
-        pass
 
     @abstractmethod
     def to_code(self) -> List[str]:
@@ -20,7 +15,22 @@ class BaseAction(ABC):
 
     @abstractmethod
     def get_used_columns(self) -> Set[str]:
+        """Returns all used columns."""
         pass
 
     def __str__(self):
         return '\n'.join(self.to_code())
+
+
+class Action(BaseComponent):
+    @abstractmethod
+    def apply(self, df: pd.DataFrame) -> pd.DataFrame:
+        """Applies logic to DataFrame."""
+        pass
+
+
+class Mask(BaseComponent):
+    @abstractmethod
+    def apply(self, df: pd.DataFrame) -> pd.Series[bool]:
+        """Applies logic to DataFrame."""
+        pass
